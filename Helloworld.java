07fe42a0b5d2980e8c2ebb756a43a0a26ec8b2c2
SakshamAry <sakshamkumar329@gmail.com>
2024-05-07 07:38:07 +0530

    Add files via upload
    My First prog of JAVA - HelloWorld

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld {
+    public static void main (String args[]){
+        System.out.println("helloWorld ");
+        System.out.print("Saksham Kumar");
+        System.out.println("How are you");
+    }
+}
