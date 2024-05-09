5025e58c0bf9c6831f7a34f801aee96666cc01ee
SakshamAry <sakshamkumar329@gmail.com>
2024-05-09 07:09:19 +0530

    Add files via upload

---
+++ Add2Num.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Add2Num {
+    public static void main(String[] args){
+
+        Scanner s = new Scanner(System.in);
+
+        int i=s.nextInt();
+        int j=s.nextInt();
+        s.close();
+
+        System.out.println(i+j);
+
+    }
+}
