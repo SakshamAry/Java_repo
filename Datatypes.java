1bea86e92f659b425bb53ae4206e2e76437fb37e
SakshamAry <sakshamkumar329@gmail.com>
2024-05-16 12:23:56 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+public class Datatypes {
+    public static void main(String args[]){
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        System.out.println(min + " " + max);
+
+        float pin = Float.MIN_VALUE;
+        float vin = Float.MAX_VALUE;
+        System.out.println(pin + " " + vin);
+        
+        char c = 'b';
+        int i = c;
+        System.out.println(i);
+
+        double d = i;
+        System.out.println(d);
+        System.out.println(i);
+
+    }
+}
